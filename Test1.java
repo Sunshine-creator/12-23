a73b024ffb5ce57f20b57e4d291cf5693c926def
Sunshine <3538545066@163.com>
2019-12-23 09:42:56 +0800

    代码练习

---
+++ Test1.java
@@ -0,0 +1,70 @@
+
+package test1;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Test1 {
+    public static void main(String[] args) {
+//       System.out.println(Integer.MAX_VALUE);
+//       System.out.println(Integer.MIN_VALUE);
+//       long num = 10L;
+//       System.out.println(num);
+//       System.out.println(Long.MAX_VALUE);
+//       System.out.println(Long.MIN_VALUE);
+//       double a =1;
+//       double b =2;
+//       System.out.println(a/b);
+//       double num =1.1;
+//       System.out.println(num * num);
+//       byte vaule = 0;
+//       System.out.println(vaule);
+//       String name ="张三";
+//       System.out.println(name);
+//       String name = "My name is \"张三\"";
+//       String a  ="hello";
+//       String b  = "world";
+//       String c  = a + b;
+//       System.out.println(c);
+//       String str = "result =";
+//       int a  =10;
+//       int b =20;
+////       String result =  str + (a + b);
+//       System.out.println("a  = " + a + " ,b= " + b);
+//       int num = 10;
+//       String str1 =  num + "";
+//       String str2= String.valueOf(num);
+//       String  str =  "100";
+//       int num =  Integer.parseInt(str);
+//       java.util.Date date =  new  java.util.Date();
+//       System.out.println(date.getTime());
+//         int [] arr= {1,2,3};
+//       System.out.println("length:" + arr.length);
+//         for(int x : arr){
+//          System.out.println(x);      /*foreach遍历数组*/
+//         }
+//         printArrary(arr);
+//        int [] arr = {1,2,3};
+//        int [] output = transform(arr);
+//        printArrary(output);
+//    }
+//    public static void  printArrary(int [] arr){
+//        for(int i =0;i<arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
+//    }
+//    public static int [] transform(int [] arr){
+//        int [] ret = new int[arr.length];
+//        for(int i =0;i<arr.length;i++){
+//            ret[i] = arr[i] *2;
+//        }
+//        return ret;
+     int []arr ={1,2,3,4,5};
+     String newArr = Arrays.toString(arr);
+     System.out.println(newArr);
+    }
+    
+}
+//public static void printArrary(int [] arr){
+//   for(int x : a){
+//        System.out.println(x);      /*foreach遍历数组*/
+//        }
+//}
